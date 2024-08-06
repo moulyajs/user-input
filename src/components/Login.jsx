2a072cb2d85bbf59e2e82860331cf4b893ea3ead
Moulya <moulya.anjanappa@gmail.com>
2024-08-06 22:16:42 +0530

    updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,17 @@
+import {useRef} from 'react';
 export default function Login() {
+
+  const email = useRef();
+  const password = useRef();
+  
   function handleSubmit (event) {
     event.preventDefault();
-    console.log("submitted");
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+    console.log(enteredEmail,enteredPassword);
+    
   }
+  
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -10,12 +19,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={password}/>
         </div>
       </div>
 
